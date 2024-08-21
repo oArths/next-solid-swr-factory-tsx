67b31aac399240684d87ea9081f5df562858d6d6
Arthur <arthurferreira1417@gmail.com>
2024-08-21 01:34:01 -0300

    Feat: cirando pagina da home

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Header  from '@/components/header/index';
+import * as s from './styles'
+
+export default function Home() {
+  return (
+  <s.Main>
+    <Header />
+    ola
+  </s.Main>  
+  );
+}
